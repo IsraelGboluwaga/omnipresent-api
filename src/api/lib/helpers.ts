de890716cd18afaf0bf8caaecb156637b1c85387
Israel Arunah <arunahisraelg2010@gmail.com>
2021-08-12 13:35:58 +0100

    Removed unused packages, corrected git links

--- src/api/lib/helpers.ts
+++
@@ -1,6 +0,0 @@
-import { isUndefined, omitBy } from 'lodash/fp'
-import { OmitByValue } from 'utility-types'
-
-const omitUndefined = <T>(obj: T) => omitBy(isUndefined, obj) as OmitByValue<T, undefined>
-
-export { omitUndefined }
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,5 @@
 declare module 'winston'
 declare module 'morgan'
-declare module 'validator'
 declare module '*.js' {
   const value: any
   export default value
